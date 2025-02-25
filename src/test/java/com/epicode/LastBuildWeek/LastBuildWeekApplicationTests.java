f16a06b94ae2a8916359989548cda9bee5d770f4
Davide De Pasquale <davide.depasquale1998@gmail.com>
2025-02-25 10:32:31 +0100

    inizio

---
+++ src/test/java/com/epicode/LastBuildWeek/LastBuildWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.LastBuildWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastBuildWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
